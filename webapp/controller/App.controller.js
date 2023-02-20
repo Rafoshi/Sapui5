0612275d54543b89817c48415f43d56c1db6cba0
Ioshi <rafa.ioshi@gmail.com>
2023-02-20 20:11:11 -0300

    estrutura das pastas

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+], function (Controller) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
 
-        onShowOi: function () {
-            MessageToast.show('oi');
-        }
     });
 })
